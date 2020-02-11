461a4a152b00d6eba08d4a71a30b3c5fe5ffbb8d
andres <andressantiagore@gmail.com>
2020-02-11 16:50:40 +0000

    add a new files

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - This function use a di
+ * @n: the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+
+int print_last_digit(int a)
+{
+	int b = a;
+
+	if (a > 0 || a == 0)
+	{
+		b = a % 10;
+		_putchar('0' + b);
+		return (b);
+	}
+	else if (a < 0)
+	{
+		b = ((a * -1) % 10);
+		_putchar('0' + b);
+		return (b);
+	}
+
+	return (b);
+}
