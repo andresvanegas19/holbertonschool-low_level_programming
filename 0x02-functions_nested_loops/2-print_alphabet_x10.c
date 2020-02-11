2f0d6cb18b3da3aa36400e72a7230dba323d4779
andres <andressantiagore@gmail.com>
2020-02-11 14:25:56 +0000

    Add a new files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,10 @@
 
 /**
  * print_alphabet_x10 - this function do is print the alphabet
-*/
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void print_alphabet_x10(void)
 {
 	int a, b;
