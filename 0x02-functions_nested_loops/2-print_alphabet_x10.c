6fed8276a23df29cc37110571642d8f21415030b
andres <andressantiagore@gmail.com>
2020-02-12 01:31:58 +0000

    Edit the file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,6 @@
 /**
  * print_alphabet_x10 - this function do is print the alphabet
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet_x10(void)
 {
