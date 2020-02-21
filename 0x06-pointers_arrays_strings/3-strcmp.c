00137f2b92b7ebfd0201220ff6d8ef813482c071
andres <andressantiagore@gmail.com>
2020-02-21 13:25:53 +0000

    fix the comments

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,11 @@
+/**
+ * _strcmp - return the comparetion of two strings
+ * @s1: pointer to text string
+ * @s2: pointer to text string
+ *
+ * Return: the comparetion
+ */
+
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0, itwo = 0;
