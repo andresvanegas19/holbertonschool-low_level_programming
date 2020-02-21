a74b7b5bc9373e9ae084749457aa549bf09b6d0e
andres <andressantiagore@gmail.com>
2020-02-21 17:43:36 +0000

    fix the return

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,13 +8,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, itwo = 0;
+	int i = 0;
 
 	while (s1[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+		{
+			s1[i] - s2[i];
+		}
 		i++;
-		itwo++;
 	}
-
-	return (*s1 - *s2);
+	return (0);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,21 +4,15 @@
  *
  * Return: they return the string to number
  */
-char *leet (char *s)
+char *leet (char *arr)
 {
-	int a, b = 0;
+	int a, b;
 
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; arr[a] != '\0'; a++)
 	{
+		for (b = 0; arr[b])
+
 	}
-	while (b < a)
-	{
-		((s[b] == 'a' || s[b] == 'A') && (s[b] = '4')) ||
-		((s[b] == 'e' || s[b] == 'E') && (s[b] = '3')) ||
-		((s[b] == 'o' || s[b] == 'O') && (s[b] = '0')) ||
-		((s[b] == 't' || s[b] == 'T') && (s[b] = '7')) ||
-		((s[b] == 'l' || s[b] == 'L') && (s[b] = '1'));
-	}
-	b++;
+
 	return (s);
 }
