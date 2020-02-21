4fc27ef898c806d330fc1d48a9bd5aa96fbba109
andres <andressantiagore@gmail.com>
2020-02-21 13:18:43 +0000

    editing the 1th file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,17 +12,18 @@ char *_strncat(char *dest, char *src, int n)
 
 	a = 0;
 	b = 0;
-	n = n;
+
 	while (dest[a] != '\0')
 	{
 		a++;
 	}
-	while (src[b] < src[n] && src[b] != '\0')
+	while (b < n && src[b] != '\0')
 	{
 		dest[a] = src[b];
 		b++;
 		a++;
 	}
+
 	dest[a] = '\0';
 	return (dest);
 }
