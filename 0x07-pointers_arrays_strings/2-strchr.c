ec2c396e0a11906245cc5eac019cf83294166f57
andres <andressantiagore@gmail.com>
2020-02-24 17:50:07 +0000

    Fix: add the commits

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,11 @@
+/**
+ * _strchr - prints when the pointer find the letter
+ * @s: the address of array
+ * @c: the letter It find
+ *
+ * Return: the rest of the string
+ */
+
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
@@ -8,7 +16,7 @@ char *_strchr(char *s, char c)
 		}
 		if (!*s != '\0')
 		{
-			return s = 00;
+			return (s = 00);
 		}
 		s++;
 	}
