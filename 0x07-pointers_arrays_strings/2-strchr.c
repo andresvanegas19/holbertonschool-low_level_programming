2bd340c0a9a53d7cbb7dada5a8e2213012097656
andres <andressantiagore@gmail.com>
2020-02-24 18:20:50 +0000

    editing

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,8 +10,7 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-	while (*s != '\0' && s[i] != '\0')
+	while (*s)
 	{
 		if (*s == c)
 		{
@@ -19,6 +18,8 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
+	if (*s == c)
+		return (s);
 
 	return (NULL);
 }
