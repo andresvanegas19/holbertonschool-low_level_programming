7491c1deebfca099515a70e25119c190925f5e72
andres <andressantiagore@gmail.com>
2020-02-25 03:25:10 +0000

    Upload: up the file

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * set_string - point to another pointer
+ * @s : pointer to wait to assign another pointer
+ * @to : pointer to the string
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = yo;
+}
