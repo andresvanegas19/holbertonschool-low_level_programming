49a5e7112443dfb3ee94e1744f64440ef469ba68
andres <andressantiagore@gmail.com>
2020-02-26 18:22:52 +0000

    Fix: editing file and upload

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,11 @@ void _puts_recursion(char *s)
 
 	if (*s != '\0')
 	{
-		s++;
-		_puts_recursion(s);
-		if (*s == '\0')
-		{
-			_putchar('\n');
-		}
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
 	}
 
 }
