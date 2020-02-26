7a92ec5add43bd0dc15cbd914160816a533b0d06
andres <andressantiagore@gmail.com>
2020-02-26 20:21:16 +0000

    Upload: add a file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+/**
+ * _pow_recursion - give the value of X raised to the power of Y
+ * @x: an integer
+ * @y: an integer
+ *
+ * Return: the value of the result
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0 )
+	{
+		return (1);
+	}
+	if (y == 1 )
+	{
+		return (x);
+	}
+	if ( y > 1)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+
+	return (0);
+}
