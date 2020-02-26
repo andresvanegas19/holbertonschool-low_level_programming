e9bfa22544077d222577161374d729f0c5135825
andres <andressantiagore@gmail.com>
2020-02-26 18:26:56 +0000

    Fix: add comments and change the return

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,14 @@
 /**
  * _strlen_recursion - give the length of a string.
  * @s: pointer to the string
+ * Return: return 0 for succes
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return 1 + _strlen_recursion((s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return;
+	return (0);
 }
