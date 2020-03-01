c71f67703b2fa6ac29e199bab5b94eb2354130d7
andres <andressantiagore@gmail.com>
2020-03-01 17:30:57 +0000

    Fix: the prototype

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
-#include "holberton.h"
 #include <stdio.h>
+
 /**
  * main - prints its own name
+ *@argc: is the number of command line arguments
+ *@argv: an array containing the program command line arguments
  *
  * Return: Always 0.
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
+
 	printf("%s\n", *argv);
 
 	return (0);
