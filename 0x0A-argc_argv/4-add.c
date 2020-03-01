c3e0716d7d21508f56181dad144d90c392e7b8dc
andres <andressantiagore@gmail.com>
2020-03-01 19:36:42 +0000

    Fix: change the output

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
 /**
  * main - adds positive numbers
  *@argc: is the number of command line arguments
@@ -14,13 +12,18 @@ int main(int argc, char *argv[])
 {
 	int sum, i;
 
-	if (argc >= 2)
+	if (argc == 2)
 	{
 		printf("0\n");
 		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
+		if (atoi(argv[i]) < 0)
+		{
+			printf("0\n");
+			return (0);
+		}
 		if (atoi(argv[i]))
 		{
 			sum += atoi(argv[i]);
