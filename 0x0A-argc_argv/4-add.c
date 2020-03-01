1ef7028b6d42d3f758de00b8cefeaf9bf0bb4e82
andres <andressantiagore@gmail.com>
2020-03-01 19:38:44 +0000

    Fix: indent code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -29,10 +29,10 @@ int main(int argc, char *argv[])
 			sum += atoi(argv[i]);
 		}
 		else
-                {
+		{
 			printf("Error\n");
-                        return (1);
-                }
+			return (1);
+		}
 
 	}
 	printf("%d\n", sum);
