3411870a22b3a175a52052213967ca271f360235
andres <andressantiagore@gmail.com>
2020-03-05 17:08:01 +0000

    Add: comments to the return

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,7 @@
  * malloc_checked  - allocates memory using malloc
  * @b: size of the memoroy b will be need
  *
+ * Return: a pointer to the reserve memory
  */
 
 void *malloc_checked(unsigned int b)
