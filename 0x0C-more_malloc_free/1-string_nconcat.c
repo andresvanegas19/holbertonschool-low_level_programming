e5d1f82d317f0f836c4e50309b0d5e0f4c862832
andres <andressantiagore@gmail.com>
2020-03-06 00:35:41 +0000

    Fix: change the logic

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,44 +11,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *pstr = NULL;
-	int i = 0, word = 0, count = 0, j = 0, size = n;
+	unsigned int i = 0, count = 0, j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[i])
 		i++;
+
 	while (s2[j])
-	{
-		if (s2[j] == ' ' && j <= size)
-			word += j;
 		j++;
-	}
-	if (size >= j)
-	{
-		pstr = malloc(sizeof(char) * (i + j + 1));
-		if (pstr == NULL)
-			return (NULL);
-	}
-	else
-	{
-		pstr = malloc(sizeof(char) * (word + i + 1));
-		if (pstr == NULL)
-			return (NULL);
-	}
+
+	if (n >= j)
+		n = j;
+
+	pstr = malloc(sizeof(char) * (i + n + 1));
+	if (pstr == NULL)
+		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
+	{
 		pstr[i] = s1[i];
-	pstr[i] = '\n';
-	if (size >= j)
-		while (s2[count])
-			pstr[i] = s2[count];
-
-	else
-		for (count = 0; count < word; count++, i++)
-			pstr[i] = s2[count];
+	}
+	for (count = 0; count < n; count++, i++)
+		pstr[i] = s2[count];
 
 	pstr[i] = '\0';
+
 	return (pstr);
 }
