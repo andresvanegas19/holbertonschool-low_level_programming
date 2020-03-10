042609da9803dc9b048c877eeec5bb78608d4b21
andres <andressantiagore@gmail.com>
2020-03-10 04:49:42 +0000

    Fix: add all function and the comments

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-int _putchar(char c);
 /**
  * struct dog - data of the dog
  *@name: dog name's
@@ -16,7 +15,15 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - Typedef for unsigned int
+ */
+typedef struct dog dog_t;
+
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
