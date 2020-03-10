40e217ca7d50c6ab491f3430f2ba145f22281cd4
andres <andressantiagore@gmail.com>
2020-03-10 04:43:05 +0000

    Fix: return the change

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 /**
  * print_dog - prints a struct dog
@@ -8,19 +9,19 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name == 0)
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", (*d).name);
 
 
-		if (d->age == 0)
+		if (d->age == 00)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", (*d).age);
 
 
-		if (d->owner == 0)
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", (*d).owner);
