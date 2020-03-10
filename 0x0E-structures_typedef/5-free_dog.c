f67d07eb034dca15655a49229922d055d6a4f9d6
andres <andressantiagore@gmail.com>
2020-03-10 05:01:01 +0000

    Fix: change the conditional

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,12 +1,13 @@
+#include <stdlib.h>
 #include "dog.h"
 /**
  * free_dog - function that frees the structure dogs.
- * @d: the structure
+ * @d: the structure of dog
  *
  */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
