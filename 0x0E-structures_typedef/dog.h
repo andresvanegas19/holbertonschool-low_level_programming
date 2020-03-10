72a7f3b9353cab21b109efafd633dfe58c73d2ea
andresvanegas19 <51345583+andresvanegas19@users.noreply.github.com>
2020-03-10 11:14:11 -0500

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,15 +14,5 @@ struct dog
 	float age;
 	char *owner;
 }
-/**
- * dog_t - Typedef for unsigned int
- */
-typedef struct dog dog_t;
-
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif /* GRANDPARENT_H */
