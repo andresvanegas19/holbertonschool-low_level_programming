8a82243f2068f265574b4270f8fe4a0c639ca221
andres <andressantiagore@gmail.com>
2020-03-10 04:35:37 +0000

    Add: adding comment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,12 @@
 
 int _putchar(char c);
 /**
- *Is the struct of dog and dates
+ * struct dog - data of the dog
  *@name: dog name's
  *@owner: dog owner's
  *@age: dog age's
+ *
+ * Description: into the struct save the charcaters of the dog
  */
 struct dog
 {
