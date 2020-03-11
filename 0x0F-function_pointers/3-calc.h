5ff479cc42c5ad7a4f316faccd7d92c111911892
andres <andressantiagore@gmail.com>
2020-03-11 20:25:51 +0000

    [Add]: add the library file

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,13 @@
+#ifndef GRANDPARENT_H
+#define GRANDPARENT_H
+
+int _putchar(char c);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * struct op - Struct op
  *
@@ -9,3 +19,5 @@ typedef struct op
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
+#endif /* GRANDPARENT_H */
