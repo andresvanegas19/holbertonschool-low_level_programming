c887f1e944868f55df8a70eb691f3297d2544f7e
andres <andressantiagore@gmail.com>
2020-03-11 17:59:56 +0000

    Inicializando el proyecto

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - recive the name and the function and return  prints a name.
+ * @name: name of the person
+ * @f: is a function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
