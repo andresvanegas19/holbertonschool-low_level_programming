969d17372a7eb3b53b66f48173b6dda13444d9ae
andres <andressantiagore@gmail.com>
2020-03-11 20:09:15 +0000

    [Add]: upload files to the calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ *@argc: is the number of command line arguments
+ *@argv: an array containing the program command line arguments
+ *
+ * Return: Always 1.
+ */
+
+int main(int argc, char *argv[])
+{
+}
