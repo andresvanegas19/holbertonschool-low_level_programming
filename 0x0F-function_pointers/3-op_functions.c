f5813938df4cb17e379c4b70c14b822cf29999b3
andres <andressantiagore@gmail.com>
2020-03-11 20:46:59 +0000

    [Add]: adding the functions to the file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - is the sum of two digits
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the sum of the two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - is the rest of two digits
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the rest of the two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - is the product of the two numbers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the product of two numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - is the division of the numbers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the division of two numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - remainder of the division of a by b
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: remainder of two numbers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
