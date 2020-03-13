504208f4052e66bcd21bfb4b9edaa6cfd8645da5
andres <andressantiagore@gmail.com>
2020-03-13 13:36:25 +0000

    [Fix]: add the conditional and the line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,10 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(printnum, int));
-
-
-			printf("%s", separator);
+			if (i != n - 1)
+				printf("%s", separator);
 		}
+		putchar('\n');
 
 		va_end(printnum);
 	}
