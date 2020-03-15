9304169319ac9e24afb72aae67e963cf51a90e68
andres <andressantiagore@gmail.com>
2020-03-15 15:01:15 +0000

    [fix]: change the conditional and add the comments

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
- * sum_them_all - returns the sum of all its parameters
+ * print_strings - prints strings, followed by a new line
+ * @separator: is the string to be printed between the strings
+ * @n: is the number of strings passed to the function
  *
- * Return: Always 0.
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -14,24 +14,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	unsigned int i = 0;
 
-	if (separator)
+	va_start(printstr, n);
+
+	for (i = 0; i < n; i++)
 	{
-		va_start(printstr, n);
+		const char *pstr = va_arg(printstr, const char*);
 
-		for (i = 0; i < n; i++)
-		{
-			const char *pstr = va_arg(printstr, const char*);
-			if (pstr != NULL)
-				printf("%s", pstr);
-			else
-				printf("(nil)");
-			if (i != n - 1)
-				printf("%s", separator);
+		if (pstr != NULL)
+			printf("%s", pstr);
+		else
+			printf("(nil)");
+		if ((i != n - 1) && separator)
+			printf("%s", separator);
 
-		}
-
-		printf("\n");
-
-		va_end(printstr);
 	}
+
+	printf("\n");
+
+	va_end(printstr);
+
 }
