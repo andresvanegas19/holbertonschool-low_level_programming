a92ba73dc4cd3ecb8c22a82b48908f3ff8908dbc
andres <andressantiagore@gmail.com>
2020-03-20 04:33:35 +0000

    [upload]: upload the new file to show a text

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("Write a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
