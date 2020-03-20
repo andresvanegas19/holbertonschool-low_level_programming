c618443ee9c49b175c15eb08fe3875897bd4664b
andres <andressantiagore@gmail.com>
2020-03-20 04:43:36 +0000

    [fix]: editing the file and change the way to print

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * text -  function that prints
  */
-int main(void)
+void text(void)__attribute__((constructor));
+void text(void)
 {
-	printf("Write a function that prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
+	printf("You're beat! and yet, you must ");
+	printf("allow,\nI bore my house upon my back!\n");
 }
