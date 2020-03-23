da39efbfc0de2b4c794edec7c45c4adfba22d484
andres <andressantiagore@gmail.com>
2020-03-23 20:27:51 +0000

    [feature]: init the 8 file and change the comment in 7th

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
- * @head: pointer to the first result
+ * get_nodeint_at_index - prints all the elements of a listint_t list
+ * @head: pointer to the first node
  * @index: is the index of the node, starting at 0
  *
  * Return: the Nth node of the list, if the node doesn't exist, return NULL
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n) of a listint_t linked list.
+ * @head: pointer to the first node
+ *
+ * Return: the Nth node of the list, if the node doesn't exist, return NULL
+ */
+
+int sum_listint(listint_t *head)
+{
+
+
+
+}
