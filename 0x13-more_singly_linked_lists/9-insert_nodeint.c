82a6331f63e77aa366bc528a00dfb2885d4b9a76
andres <andressantiagore@gmail.com>
2020-03-23 20:58:03 +0000

    [upload]: built the main function and add the conditional

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: pointer to point the first node
+ * @idx: is the index of the list where the new node should be added.
+ * @n: is the number to fill the node
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newNode, *pHead = *head;
+	unsigned int count = 0;
+
+	if (idx == 0)
+	{
+		newNode = malloc(sizeof(listint_t));
+		newNode->n = n;
+		newNode->next = pHead->next;
+		pHead->next = newNode;
+		return (newNode);
+	}
+
+	for (; pHead; pHead = pHead->next, count++)
+		if (count == (idx - 1))
+		{
+			newNode = malloc(sizeof(listint_t));
+			newNode->n = n;
+			newNode->next = pHead->next;
+			pHead->next = newNode;
+			return (newNode);
+		}
+
+	return (NULL);
+}
