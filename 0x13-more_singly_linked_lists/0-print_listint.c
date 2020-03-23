78ea92d96f92113911390fc51bfac30dc608da23
andres <andressantiagore@gmail.com>
2020-03-23 15:03:20 +0000

    [delete]: delete the execute file and change the indentation of 1 file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,4 @@
+#include "lists.h"
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: contents of linked list starting from the given node
@@ -6,15 +7,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    int count;
+	int count = 0;
 
-    while (h)
-    {
-        if (h->str)
-            printf("%d\n", h->n);
-        h = h->next;
-        count++; 
-    }
+	while (h)
+	{
+		if (h->n)
+			printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len -  returns the number of elements in a linked listint_t list.
+ * @h: contents of linked list starting from the given node
+ *
+ * Return: number of elements in a linked.
+ */
+ size_t listint_len(const listint_t *h)
+ {
+    int count = 0;
+
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
+ }
