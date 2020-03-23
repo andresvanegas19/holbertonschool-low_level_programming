e82eba679e7b2a952c64bdb6e58e3738bf0601cd
andres <andressantiagore@gmail.com>
2020-03-23 14:46:11 +0000

    [feature] add the file the main function and add the libraries

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: contents of linked list starting from the given node
+ *
+ * Return: the number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+    int count;
+
+    while (h)
+    {
+        if (h->str)
+            printf("%d\n", h->n);
+        h = h->next;
+        count++; 
+    }
+
+    return (count);
+}
