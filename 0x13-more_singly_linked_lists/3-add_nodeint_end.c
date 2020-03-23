a5a8a16016ed341d6d97ff18e4644012a53fede4
andres <andressantiagore@gmail.com>
2020-03-23 15:57:59 +0000

    [fix]: add the comments

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
+ * add_nodeint_end - adds a new node at the end of a listint_t list.
  * @head: contents of linked list starting from the given node
  *  @n: is the data to fill the new node
  *
