2582a9e1ed9942ddb3b7e52c56391bae8f578345
andres <andressantiagore@gmail.com>
2020-03-23 15:31:30 +0000

    [delete]: change the indentation to the function and delete the execute file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 /**
- * main - check the code for Holberton School students.
- * @h: contents of linked list starting from the given node
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: contents of linked list starting from the given node
  * @n: is the data to fill the new node
  *
  * Return: the address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-     listint_t *newNode = malloc(sizeof(listint_t));
-     if (newNode == NULL)
-          return (NULL);
+	listint_t *newNode = malloc(sizeof(listint_t));
 
-     newNode->n = n;
-     newNode->next = *head;
-     *head = newNode;
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
 
-     return (newNode);
+	return (newNode);
 }
