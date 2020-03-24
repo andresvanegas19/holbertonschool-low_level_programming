6b5761f6675ddfb4fffae42609fe8f64f0d83f55
andres <andressantiagore@gmail.com>
2020-03-24 18:50:06 +0000

    [fix]: delete the connection of the function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->n = n;
 		*head = newNode;
 		newNode->next = NULL;
-		pHead->next = newNode;
 		return (newNode);
 	}
 	if (idx == 0)
