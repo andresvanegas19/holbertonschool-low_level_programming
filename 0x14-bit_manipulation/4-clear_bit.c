b3a23ccb7d1294c7e6b3adc330a412d9c281f94e
andres <andressantiagore@gmail.com>
2020-03-27 00:20:10 +0000

    [upload]: built the main function and return 1 4th

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * set_bit - sets the value of a bit to 1 at a given index.
+ * clear_bit - sets the value of a bit to 0 at a given index.
  * @index: is the index, starting from 0 of the bit you want to set
  * @n: is the number.
  *
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	*n = *n | (1 << index);
-	return (1);
+	*n &= ~(1 << index);
+    return (1);
 }
