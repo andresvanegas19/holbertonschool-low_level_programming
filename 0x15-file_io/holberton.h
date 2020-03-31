f8e198cff602b6d3ea287e84bca42e56cb58627c
andres <andressantiagore@gmail.com>
2020-03-31 18:54:51 +0000

    [add]: initialization the 15 task of I/O

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,15 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+/*library*/
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
+/*prototype*/
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+#endif
