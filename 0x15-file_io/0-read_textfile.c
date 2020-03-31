9f0eabf8162f8b68231dbba17289753f4a84dff3
andres <andressantiagore@gmail.com>
2020-03-31 19:03:08 +0000

    [update]: built the main file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * @letters: is the number of letters it should read and print
+ * @filename: is the file will read the output
+ *
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,8 @@
 #define MY_HEADER_FILE_H
 
 /*library*/
+#include <stdio.h>
+#include <stdlib.h>
 #include<fcntl.h>
 #include<sys/types.h>
 #include<sys/stat.h>
@@ -10,6 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 
 #endif
