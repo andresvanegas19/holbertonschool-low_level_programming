82bd7d852afad56b07cd0b4fefb1bf226d06116a
andres <andressantiagore@gmail.com>
2020-03-31 22:08:20 +0000

    [fix]: change the main function of th 0 task and init the 1th function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * create_file - function that creates a file.
+ *
+ * Return: Always 0.
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+	if (filename)
+		return (0);
+
+}
