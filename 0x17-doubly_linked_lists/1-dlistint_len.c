db930a308caeee4929aaf964c25f64cd84f1cc96
Andres reyes <andressantiagore@gmail.com>
2020-05-01 12:20:52 -0500

    [upload]: add a new file

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ *
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *pHDouble;
+    int i;
+
+    if (h == NULL)
+        return (0);
+
+    for (pHDouble = h, i = 0; pHDouble; pHDouble = pHDouble->next, i++)
+        ;
+
+    return (i);
+}
---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *tailHead = *head, *newNode;
+
+    newNode = malloc(sizeof(dlistint_t));
+    if (newNode == NULL)
+        return (NULL);
+    newNode->next = NULL, newNode->prev = NULL;
+    newNode->n = n;
+
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return (*head);
+    }
+
+    for (; tailHead; tailHead = tailHead->next)
+        ;
+
+    newNode->prev = tailHead;
+    tailHead->next = newNode;
+
+    return (newNode);
+}
