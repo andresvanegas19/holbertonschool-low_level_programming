5c6eec3c5708558b97484d8ef8322653c576a2dc
Andres reyes <andressantiagore@gmail.com>
2020-05-01 11:54:55 -0500

    [upload]: built the first function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,25 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: print the whole linked list
  *
- * Return: Always EXIT_SUCCESS.
+ * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    
+	const dlistint_t *pDoublelist;
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	pDoublelist = h;
+	while(pDoublelist)
+	{
+		printf("%d\n", pDoublelist->n);
+		pDoublelist = pDoublelist->next;
+		count++;
+	}
+	return(count);
 }
