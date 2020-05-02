f706db9ccdba4f4b83d5fb2ae607141f2188ef31
Andres reyes <andressantiagore@gmail.com>
2020-05-02 13:07:42 -0500

    [fix]: add the style of the function

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,9 +4,10 @@
  * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t
  * linked list.
  *
+ * @head: is a double pointer to point a double linked list
  * @index: is the index of the node that should be deleted. Index starts at 0
  *
- * Returns: 1 if it succeeded, -1 if it failed
+ * Return: 1 if it succeeded, -1 if it failed
  *
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
