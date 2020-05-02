f4236891cf58d3e80770ebf80d473508b50003e0
Andres reyes <andressantiagore@gmail.com>
2020-05-02 13:04:09 -0500

    [upload]: finish the function

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,11 +12,10 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *pHead = *head, *delete;
-	unsigned int count = 1;
+	unsigned int count = 0;
 
 	if (*head == NULL)
 		return (-1);
-
 	if (index == 0)
 	{
 		if ((*head)->next != NULL)
@@ -24,9 +23,32 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			delete = *head;
 			*head = (*head)->next;
 			(*head)->prev = NULL;
+			delete->next = NULL;
+			free(delete);
 		}
-		free(*head);
+		else
+		{
+			*head = NULL;
+			free(*head);
+		}
 		return (1);
 	}
-
+	for (; pHead && count <= index; count++, pHead = pHead->next)
+	{
+		if (count == index)
+		{
+			delete = pHead;
+			if (pHead->next != NULL)
+			{
+				pHead->prev->next = pHead->next;
+				pHead->next->prev = pHead->prev;
+			}
+			else
+				pHead->prev->next = NULL;
+			delete->prev = NULL, delete->next = NULL;
+			free(delete);
+			return (1);
+		}
+	}
+	return (-1);
 }
