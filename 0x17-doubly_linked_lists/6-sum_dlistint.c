05dd40a3919520dc5782d85dc7efde5b63a03fb8
Andres reyes <andressantiagore@gmail.com>
2020-05-02 00:46:57 -0500

    [upload]: add a file to conect the nodes

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - the sum of all the data (n) of a dlistint_t linked list.
+ *
+ * Return: sum of all the data, if the list is empty, return 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *pHead = head;
+    int total = 0;
+
+    if (head == NULL)
+        return (0);
+
+    while(pHead)
+    {
+        total += pHead->n;
+        pHead = pHead->next;
+    }
+
+    return (total);
+}
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index -  inserts a new node at a given position.
+ * @idx:  the list where the new node should be added.
+ * @h: is the pointer to a double linked list
+ * @n: is
+ *
+ * Return: he address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *pHead = *h, *newNode;
+	unsigned int n;
+	int count;
+
+	if (*h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		add_dnodeint(&(*h), n);
+		return (*h);
+	}
+
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	while(pHead && count <= idx)
+	{
+		if (count == idx)
+		{
+			if (pHead->next == NULL)
+			{
+				newNode = add_dnodeint_end(&(*h), 4);
+				return (newNode);
+			}
+			else
+			{
+
+				return (newNode);
+			}
+		}
+		count++;
+		pHead = pHead->next;
+	}
+	return(NULL);
+}
