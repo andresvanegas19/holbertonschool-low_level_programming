ca3bbfc4335ece4ff5c869039bd1520b4bfcbfd8
Andres <andressantiagore@gmail.com>
2020-05-04 23:57:32 +0000

    [upload]: make the object file to use in python

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,58 @@
+/**
+ * sum - sum two parameters
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the sum
+ */
+int add(int a, int b)
+{
+  return (a + b);
+}
+
+/**
+ * sub - substaction of  two parameters
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the substraction of two numbers
+ */
+int sub(int a, int b)
+{
+  return (a + b);
+}
+/**
+ * mul - multiplication of  two parameters
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the multiplication of two numbers
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}
+
+/**
+ * div - division two parameters
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the division of two numbers
+ */
+int div(int a, int b)
+{
+  return (a / b);
+}
+
+/**
+ * mod -  modular of two parameters
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: modular of two numbers
+ */
+int mod(int a, int b)
+{
+  return (a % b);
+}
