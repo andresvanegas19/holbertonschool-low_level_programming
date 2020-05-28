120f5427a9de4b5e6a346369f3e94157de3b86c8
andres reyes <andressantiagore@gmail.com>
2020-05-28 18:20:22 -0500

    upload the changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,13 +1,23 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table.
- * @size: is the size of the hash table.
+ * hash_table_set - adds an element to the hash table.
+ * @ht: is the hash table.
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key. value must be duplicated.
+ * value can be an empty string
  *
- * Return: the has table if not return NULL
+ * Return: 1 if it succeeded, 0 otherwise
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int indx = 0;
+
+	if (key == NULL || value == NULL)
+		return (0);
+
+	indx = key_index(key, ht->size);
+
 
 
 
