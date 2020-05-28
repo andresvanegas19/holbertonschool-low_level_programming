683f3b87e36c72b973abb33831d06bf73e78a162
andres reyes <andressantiagore@gmail.com>
2020-05-28 15:46:40 -0500

    change the files, upload the git igonore

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table.
+ * @size: is the size of the hash table.
+ *
+ * Return: the has table if not return NULL
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t = NULL;
+
+	if (size == 0)
+		return(NULL);
+
+	hash_t =  malloc(sizeof(hash_table_t));
+	hash_t->size = size;
+	hash_t->array = malloc(sizeof(hash_node_t) * size);
+
+	return (hash_t);
+}
