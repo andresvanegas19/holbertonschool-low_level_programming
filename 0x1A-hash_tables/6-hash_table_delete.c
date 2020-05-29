4cc4c968429fc8ce1d58fbdae6c7398e72df8822
andres reyes <andressantiagore@gmail.com>
2020-05-29 16:21:51 -0500

    change the variable

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -14,13 +14,13 @@ void hash_table_delete(hash_table_t *ht)
 	for (i = 0; i <= (int) ht->size; i++)
 	{
 	/* it will not pass from here if it is null*/
-		for (; ht->array[indx] != NULL;)
+		for (; ht->array[i] != NULL;)
 		{
-			pHt = ht->array[indx]->next;
-			free(ht->array[indx]->value);
-			free(ht->array[indx]->key);
-			free(ht->array[indx]);
-			ht->array[indx] = pHt;
+			pHt = ht->array[i]->next;
+			free(ht->array[i]->value);
+			free(ht->array[i]->key);
+			free(ht->array[i]);
+			ht->array[i] = pHt;
 		}
 	}
 	free(ht->array);
