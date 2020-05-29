ffe108c0546835e11a5785483a65f1faa9a49ec0
andres reyes <andressantiagore@gmail.com>
2020-05-29 11:07:42 -0500

    not open space in the heap

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_t->size = size;
-	hash_t->array = calloc(size, sizeof(hash_node_t));
+	hash_t->array = NULL;
 	if (hash_t == NULL)
 		return (NULL);
 
