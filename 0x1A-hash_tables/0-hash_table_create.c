804ba68d62de2c306e70318a4c811ef110db74c0
andres reyes <andressantiagore@gmail.com>
2020-05-29 14:17:17 -0500

    upload the core dump

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,13 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_t = NULL;
 	unsigned long int i = 0;
+	hash_table_t *hash_t;
 
 	if ((int)size <= 0)
 		return (NULL);
 
-	hash_t =  malloc(sizeof(hash_table_t));
+	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL)
 		return (NULL);
 
