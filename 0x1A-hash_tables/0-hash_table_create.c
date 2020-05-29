c1b7dda493724fb0def5a43de564a5e450c0271e
andres reyes <andressantiagore@gmail.com>
2020-05-29 11:11:22 -0500

    open space and cast diferent

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hash_t->size = size;
-	hash_t->array = NULL;
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_t == NULL)
 		return (NULL);
 
