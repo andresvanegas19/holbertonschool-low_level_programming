6c6ae60e6b2b733cb5d66886c8f23cf630e61e48
andres reyes <andressantiagore@gmail.com>
2020-05-29 16:07:56 -0500

    uploat the function

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,21 +9,20 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	int i = 0;
+	hash_table_t *pHt;
 
 	for (i = 0; i <= (int) ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+	/* it will not pass from here if it is null*/
+		for (; ht->array[indx] != NULL;)
 		{
-			if (ht->array[i]->next == NULL)
-				printf("perro\n");
-			else
-			{
-				printf("A");
-
-			}
-
-
+			pHt = ht->array[indx]->next;
+			free(ht->array[indx]->value);
+			free(ht->array[indx]->key);
+			free(ht->array[indx]);
+			ht->array[indx] = pHt;
 		}
 	}
-
+	free(ht->array);
+	free(ht);
 }
