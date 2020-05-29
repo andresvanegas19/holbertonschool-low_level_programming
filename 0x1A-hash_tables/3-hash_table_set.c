6e6e523fbb65af0a641c64992568aa011ff7a97b
andres reyes <andressantiagore@gmail.com>
2020-05-29 13:57:56 -0500

    refactor the function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 
+hash_node_t create_new_hash(char key, char value);
 /**
  * hash_table_set - adds an element to the hash table.
  * @ht: is the hash table.
@@ -14,41 +15,66 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int indx = 0;
 	hash_node_t *new_element = NULL;
 
-
 	if (key == NULL || value == NULL)
 		return (0);
+
 	indx = key_index((const unsigned char *)key, ht->size);
+/*
 	if (ht->array[indx] != NULL)
 		if (strcmp(ht->array[indx]->key, key) == 0)
 		{
 			ht->array[indx]->value = (char *) value;
 			return (1);
 		}
-	new_element = malloc(sizeof(hash_node_t));
-	new_element->key = (char *) key;
-	new_element->value = (char *) value;
-	new_element->next = NULL;
-	if (ht->array[indx] != NULL)
-	{
-		while (ht->array[indx] != NULL)
-		{
-			if (strcmp(ht->array[indx]->key, key) == 0)
-			{
-				free(new_element->value), free(new_element->key);
-				free(new_element);
-				ht->array[indx]->value = (char *) value;
+*/
+	new_element = create_new_hash((char *) key, (char *) value);
 
-				return (1);
-			}
-			if (ht->array[indx]->next == NULL)
-			{
-				ht->array[indx]->next = new_element;
-				return (1);
-			}
-			ht->array[indx] = ht->array[indx]->next;
-		}
+	if (ht->array[indx] == NULL)
+	{
+		ht->array[indx] = new_element;
+		return (1);
 	}
+
+/*	if (ht->array[indx] != NULL)*/
+	while (ht->array[indx] != NULL)
+	{
+		if (strcmp(ht->array[indx]->key, key) == 0)
+		{
+			free(new_element->value), free(new_element->key);
+			free(new_element);
+			ht->array[indx]->value = (char *) value;
+			return (1);
+		}
+		if (ht->array[indx]->next == NULL)
+		{
+			ht->array[indx]->next = new_element;
+			return (1);
+		}
+		ht->array[indx] = ht->array[indx]->next;
+	}
+
 	ht->array[indx] = new_element;
 
 	return (1);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: is the hash table.
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key. value must be duplicated.
+ * value can be an empty string
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+*/
+hash_node_t create_new_hash(char key, char value)
+{
+	hash_node_t *new_element;
+
+	new_element = malloc(sizeof(hash_node_t));
+	new_element->key = key;
+	new_element->value = value;
+	new_element->next = NULL;
+
+	return (new_element);
 }
