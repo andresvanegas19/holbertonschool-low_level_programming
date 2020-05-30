e5b697e7f503d28ea1e361270f7210be5d99f201
andres reyes <andressantiagore@gmail.com>
2020-05-29 21:11:52 -0500

    add the comments

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - creates a hash table.
- * @size: is the size of the hash table.
+ * hash_table_delete - deletes a hash table.
+ * @ht: is the hash table.
  *
  * Return: the has table if not return NULL
 */
